b2161278b64377dcf95ede0f977f70a6df92d888
alireza mohammadi <ar_mohammady@live.com>
2023-08-19 14:06:07 +0330

    first

--- app/Http/Controllers/CallController.php
+++ app/Http/Controllers/CallController.php
@@ -56,7 +56,7 @@ class CallController extends Controller
             'status_code' => 0,
             'message'     => 'success',
             'data'        => [
-                'CallerId' => $request -> input('CalledNumber'),
+                'caller_id' => $request -> input('CallerId'),
                 'voice' => "mediana01.wav",
             ],
         ]);
@@ -128,7 +128,7 @@ class CallController extends Controller
                     2 => $redirect -> backup_redirect_phone_number,
                     default => $redirect -> redirect_phone_number
                 },
-                'CallerId' => $request -> input('CalledNumber'),
+                'caller_id' => $request -> input('CallerId'),
             ],
         ]);
     }
