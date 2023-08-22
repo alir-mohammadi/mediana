e1c05a65834e453a17e046e8ee57121b56e46a9e
alireza mohammadi <ar_mohammady@live.com>
2023-08-22 14:47:44 +0330

    first

--- config/voice.php
+++ config/voice.php
@@ -4,6 +4,7 @@ return [
     2=> 'lavender-afra04.wav',
     6=> 'lavender-afra04.wav',
     5 =>'afra-filarmo-02.wav',
-    7 => 'irv-mediana-phone.mp3',
-    4 => 'irv-mediana-phone.mp3',
+    7 => 'ivr-mediana-phone.mp3',
+    4 => 'ivr-mediana-phone.mp3',
+    8 => 'ivr-mediana-phone.mp3',
 ];
