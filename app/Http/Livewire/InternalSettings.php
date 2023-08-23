708595b8fce3731d822f43aa6a0a88d583270435
alireza mohammadi <ar_mohammady@live.com>
2023-08-23 20:42:39 +0330

    first

--- app/Http/Livewire/InternalSettings.php
+++ app/Http/Livewire/InternalSettings.php
@@ -48,7 +48,7 @@ class InternalSettings extends Component
                 'backup' => 'nullable|numeric|digits:11',
             ]);
             if ($validate->fails()) {
-                $this->emit('toastMessage', 'error', 'شماره وارد شده نامعتبر است.');
+                $this->emit('toastMessage', 'error', ["message" =>'شماره وارد شده نامعتبر است.']);
                 return;
             }
            $line->redirects()->updateOrCreate(
