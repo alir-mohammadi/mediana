d1140c4dfa6bac05884f3d87a7b13cbf67a92db8
alireza mohammadi <ar_mohammady@live.com>
2023-09-04 17:12:41 +0330

    first
    Signed-off-by: alireza mohammadi <ar_mohammady@live.com>

--- config/voice.php
+++ config/voice.php
@@ -3,6 +3,7 @@ return [
     1 => 'mediana01.wav',
     2=> 'lavender-afra04.wav',
     6=> 'lavender-afra04.wav',
+    4 => 'ivr-mediana-phone.mp3',
     5 =>'afra-filarmo-02.wav',
     7 => 'ivr-mediana-phone.mp3',
     4 => 'ivr-mediana-phone.mp3',
