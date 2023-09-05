8d2c9b0885a740cd71382eb75d9fbc56d22bf7c9
alireza mohammadi <ar_mohammady@live.com>
2023-09-05 18:47:57 +0330

    first
    Signed-off-by: alireza mohammadi <ar_mohammady@live.com>

--- config/voice.php
+++ config/voice.php
@@ -1,8 +1,8 @@
 <?php
 return [
     1 => 'mediana01.wav',
-    2=> 'lavender-afra04.wav',
-    6=> 'lavender-afra04.wav',
+    2 => 'lavender-afra04.wav',
+    6 => 'lavender-afra04.wav',
     4 => 'ivr-mediana-phone.mp3',
     5 =>'afra-filarmo-02.wav',
     7 => 'ivr-mediana-phone.mp3',
@@ -12,4 +12,7 @@ return [
     11 => 'mediana01.wav',
     12 => 'mediana01.wav',
     13 => 'ivr-moshasha-91340074.mp3',
+    15 => 'ivr-ogift-91340075.mp3',
+    14 => 'ivr-mehrshop-91340076.mp3',
+    17 => 'ivr-meysamishop-91340078.mp3'
 ];
